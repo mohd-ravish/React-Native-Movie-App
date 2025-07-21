fd0bd90daf75a8d86d0a65e11a6d67e5a6e4a1e2
mohd-ravish <mohdravish2003@gmail.com>
2025-07-21 18:01:32 +0530

    implement search functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,31 +1,50 @@
 import { icons } from '@/constants/icons';
 import React from 'react';
-import { Image, TextInput, TouchableOpacity } from 'react-native';
+import { Image, TextInput, TouchableOpacity, View } from 'react-native';
 
 interface Props {
-    onPress?: () => void;
     placeholder: string;
+    onPress?: () => void;
+    value?: string;
+    onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ onPress, placeholder }: Props) => {
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
+    // If onPress is provided, it's a non-editable search bar that navigates.
+    if (onPress) {
+        return (
+            <TouchableOpacity
+                className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'
+                onPress={onPress}
+                activeOpacity={0.8}
+            >
+                <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+                <TextInput
+                    placeholder={placeholder}
+                    placeholderTextColor='#a8b5db'
+                    className='flex-1 ml-2 text-white'
+                    editable={false}
+                    pointerEvents="none"
+                />
+            </TouchableOpacity>
+        )
+    }
+
+    // Otherwise, it's an editable search bar.
     return (
-        <TouchableOpacity
+        <View
             className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'
-            onPress={onPress}
-            activeOpacity={0.8}
         >
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
             <TextInput
                 placeholder={placeholder}
-                value=''
-                onChange={() => { }}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor='#a8b5db'
                 className='flex-1 ml-2 text-white'
-                editable={false}
-                pointerEvents="none"
             />
-        </TouchableOpacity>
+        </View>
     )
 }
 
-export default SearchBar
+export default SearchBar;
